1a0a08f769c17b96a3bf8e7e3009f1bd6a6093c6
Pedro Henrique <pedrooheenrique159@gmail.com>
2021-06-16 23:41:15 -0300

    First commit

---
+++ NumerosPorExtenso/Program.cs
@@ -0,0 +1,28 @@
+﻿using Caelum.Stella.CSharp.Inwords;
+using System.Diagnostics;
+
+namespace NumerosPorExtenso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double valor = 75;
+            string extenso = new Numero(valor).Extenso();
+            Debug.WriteLine(extenso);
+
+            valor = 1532987;
+            extenso = new Numero(valor).Extenso();
+            Debug.WriteLine(extenso);
+
+            Debug.WriteLine(extenso + " Reais");
+
+            string extensoBRL = new MoedaBRL(valor).Extenso();
+            Debug.WriteLine(extensoBRL);
+
+            valor = 1532987.89;
+            extensoBRL = new MoedaBRL(valor).Extenso();
+            Debug.WriteLine(extensoBRL);
+        }
+    }
+}
